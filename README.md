5798ad76cb7e05a39cc4913b34cd2220f8481a83
zfhe <2825841950@qq.com>
2023-11-28 08:30:13 +0800

    feat:实现求解满秩分解函数

---
+++ README.md
@@ -0,0 +1,2 @@
+# 矩阵论（Matrix Theory）
+用Python实现矩阵论中的相关计算，如果code有误，还请提一下issue
---
+++ matrix_factorizations/full_rank.py
@@ -0,0 +1,36 @@
+import numpy as np
+from sympy import Matrix
+
+'''
+Full-Rank Factorization
+parameter: A Matrix
+return: F, G Matrix
+'''
+def full_rank(A):
+    r = A.rank()
+    A_arr1 = np.array(A.tolist())
+    # 求解A的最简行阶梯矩阵，要转换成list，再转换成array
+    A_rref = np.array(A.rref()[0].tolist())
+    k = [] # 存储被选中的列向量的下标
+    # 遍历A_rref的行
+    for i in range(A_rref.shape[0]):
+        # 遍历A_rref的列
+        for j in range(A_rref.shape[1]):
+            # 遇到1就说明找到了A矩阵的列向量的下标
+            # 这些下标的列向量组成F矩阵，然后再找下一行
+            if A_rref[i][j] == 1:
+                k.append(j)
+                break
+    # 通过选中的列下标，构建F矩阵       
+    F = Matrix(A_arr1[:,k])
+    # G就是取行最简行矩阵A的前r行构成的矩阵
+    G = Matrix(A_rref[:r])
+    print("F:", F)
+    print("G:", G)
+    return F, G
+
+if __name__ == "__main__":
+    # 表示矩阵A
+    A = np.array([[1, 1, 0], [0, 1, 1], [-1, 0, 0], [1, 1, 1]])
+    A = Matrix(A)
+    F, G = full_rank(A)
